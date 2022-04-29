eb161606506f32df0c58ff564760b375807327de
Aayush2502 <waghchaureaayush-fe@atharvacoe.ac.in>
2022-04-29 23:40:25 +0530

    update post recipes and added login sign up page

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import json
 
 
-with open('config.json', 'r') as c:
+with open('HTML\Flask Mini Project\config.json', 'r') as c:
     params = json.load(c)["params"]
 
 
@@ -23,8 +23,26 @@ class Recipes(db.Model):
     slug = db.Column(db.String(50), nullable=False)
     user = db.Column(db.String(50), nullable=True)
     description = db.Column(db.String(2000), nullable=False)
+    prep_time = db.Column(db.String(50), nullable=False)
+    cook_time = db.Column(db.String(50), nullable=False)
+    serv = db.Column(db.String(50), nullable=False)
     content = db.Column(db.String(120000000), nullable=False)
+    ingre = db.Column(db.String(50), nullable=False)
+    tip = db.Column(db.String(50), nullable=False)
     date = db.Column(db.String(12), nullable=True)
+
+
+class proce(db.Model):
+    slug = db.Column(db.String(50), nullable=False)
+    step = db.Column(db.Integer, primary_key=True)
+    proc = db.Column(db.String(1000), nullable=False)
+
+
+class Ingredients(db.Model):
+    srno = db.Column(db.Integer, primary_key=True)
+    slug = db.Column(db.String(50), nullable=False)
+    ingredient = db.Column(db.String(50), nullable=False)
+    amount = db.Column(db.String(50), nullable=False)
 
 
 @app.route("/")
@@ -45,9 +63,9 @@ def dashboard():
             # set the session variable
             session['user'] = username
             ###posts = Posts.query.all()
-            return render_template('dashboard.html', params=params)
+            return render_template('rohan2 profile page/index.html', params=params)
 
-    return render_template('login.html', params=params)
+    return render_template('rohan1 login page/index.html', params=params)
 
 
 @app.route("/post-recipes", methods=['GET', 'POST'])
@@ -55,14 +73,25 @@ def post_recipes():
     if request.method == 'POST':
         box_title = request.form.get('title')
         description = request.form.get('description')
-        slug = request.form.get('slug')
+        slug = box_title.replace(' ', '-')
+        prep_time = request.form.get('prep-time')
+        cook_time = request.form.get('cook-time')
+        serv = request.form.get('serv')
         content = request.form.get('content')
+        ingre = request.form.get('ingre')
+        tip = request.form.get('tip')
         date = datetime.now()
-        recipe = Recipes(title=box_title, slug=slug,
-                         content=content, description=description, date=date)
+        recipe = Recipes(title=box_title, slug=slug, prep_time=prep_time, cook_time=cook_time,
+                         serv=serv, ingre=ingre, tip=tip, content=content, description=description, date=date)
         db.session.add(recipe)
         db.session.commit()
     return render_template('post-recipes.html')
+
+
+@app.route("/post/<string:post_slug>", methods=['GET'])
+def post_route(post_slug):
+    post = Recipes.query.filter_by(slug=post_slug).first()
+    return render_template('post.html', post=post)
 
 
 @app.route("/about")
